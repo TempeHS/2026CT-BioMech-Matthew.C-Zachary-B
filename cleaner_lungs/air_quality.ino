1a6e616f35aa59bbd670b3e73d5ec55ec5e98d3b
Zachary B <zachary.booth3@education.nsw.gov.au>
2025-11-17 12:48:48 +1100

    add air quality md and ino code

---
+++ cleaner_lungs/air_quality.ino
@@ -0,0 +1,34 @@
+#include <Air_Quality_Sensor.h>
+
+void air_quality_setup() {
+  AirQualitySensor sensor(A0);
+  while (!Serial)
+    ;
+  Serial.println("Waiting sensor to init...");
+  delay(20000);
+
+  if (sensor.init()) {
+    Serial.println("Sensor ready.");
+  } else {
+    Serial.println("Sensor ERROR!");
+  }
+}
+
+void air_quality() {
+  int quality = sensor.slope();
+
+  Serial.print("Sensor value: ");
+  Serial.println(sensor.getValue());
+
+  if (quality == AirQualitySensor::FORCE_SIGNAL) {
+    Serial.println("High pollution! Force signal active.");
+  } else if (quality == AirQualitySensor::HIGH_POLLUTION) {
+    Serial.println("High pollution!");
+  } else if (quality == AirQualitySensor::LOW_POLLUTION) {
+    Serial.println("Low pollution!");
+  } else if (quality == AirQualitySensor::FRESH_AIR) {
+    Serial.println("Fresh air.");
+  }
+
+  delay(1000);
+}
